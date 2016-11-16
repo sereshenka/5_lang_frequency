38c8977591b168e56f2b364dd8f1bffadad7fd8e
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-11-16 23:56:53 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -15,43 +15,33 @@ def load_win_unicode_console():
         win_unicode_console.enable()
 
 
-def read_arguments():
-    """
-    использую с join,чтобы программа работала при указании пути,в котором папка
-    может содержать в названии пробел.(C:\\Users\\New User\\file.format)
-    """
+def input_path():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--direction', help='Укажите путь к файлу ', nargs = '+')
-    arguments = parser.parse_args().direction
-    try :
-        file_path = ' '.join(arguments)
-    except TypeError:
-        return None, parser
-    return file_path, parser  
+    parser.add_argument('--path', help='Укажите путь к папке ', nargs = '?')
+    file_path = parser.parse_args().path
+    if file_path is None:
+        parser.print_help()
+    return file_path  
 
         
 def load_data(file):
-    if not os.path.exists(file):
-        return None
-    else:
-        with open(file, 'r', encoding = 'utf-8') as file_handler:
-            return file_handler.read().lower()
+    with open(file, 'r', encoding = 'utf-8') as file_handler:
+        return file_handler.read().lower()
     
         
-def get_most_frequent_words(text):
+def get_all_words_in_text(text):
     p = re.compile("([a-zA-Zа-яА-Я-']+)")
     all_words = p.findall(text)
-    return Counter(all_words)
-    """
-    можно было сразу найти 10 самых популярных слов:
-    return Counter(all_words).most_common(10)
-    но это будет не рационально,тк в других проектах,
-    легче будет использовать эту функию и сделать выборку,если нужна
-    """
+    return all_words
 
 
-def print_most_10_words(top10_of_dictionary):
-    for indx, item in enumerate(top10_of_dictionary):
+def get_top10_words(all_words):
+    dictionary = Counter(all_words)
+    return dictionary.most_common(10)
+    
+    
+def print_result(top10_words):
+    for indx, item in enumerate(top10_words):
         print(("{}. {} - {} раз(а)").format(indx+1, item[0], item[1]))
 
 
@@ -59,9 +49,11 @@ if __name__ == '__main__':
     while True:
         load_win_unicode_console()
         
-        file_path,parser = read_arguments()
+        file_path = input_path()
         if file_path is None:
-            parser.print_help()
+            break
+        if not os.path.exists(file_path):
+            print('Неправильно указан путь\папка не существует')
             break
         
         try:
@@ -69,11 +61,8 @@ if __name__ == '__main__':
         except ValueError:
             print('Фаил нельзя прочитать(неподдерживаемый формат фаила)')
             break
-        if text is None:
-            print ('Неверный путь до файла\файла не существует,перезапустите программу и введите правильные данные')
-            break
         
-        dictionary = get_most_frequent_words(text)
-        top10_of_dictionary = dictionary.most_common(10)
-        print_most_10_words(top10_of_dictionary)
+        all_words = get_all_words_in_text(text)
+        top10_words = get_top10_words(all_words)
+        print_result(top10_words)
         break
